f84c715dad84ec327283c6368f8ff39560f25c34
Paul Halliday <paulhalliday@pauls-mbp.home>
2017-12-20 00:54:37 +0000

    Added Calendar and downgraded TypeScript

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -2,9 +2,10 @@ import { NgModule, Optional, SkipSelf, NO_ERRORS_SCHEMA } from '@angular/core';
 // app
 import { HomeComponent } from './components/home/home.component';
 import { SHARED_MODULES } from './home.common';
-
+import { CalendarModule } from '@progress/kendo-angular-dateinputs'
 @NgModule({
     imports: [
+        CalendarModule,
         ...SHARED_MODULES
     ],
     declarations: [HomeComponent]
