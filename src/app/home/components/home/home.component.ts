1d26840ef6c8d4b21121ee616c95c2a94d065dcc
Paul Halliday <paulhalliday@pauls-mbp.home>
2017-12-20 01:16:08 +0000

    Added selected date.

--- src/app/home/components/home/home.component.ts
+++ src/app/home/components/home/home.component.ts
@@ -10,6 +10,10 @@ export class HomeComponent implements OnInit {
     // Focused date
     focusedDate: Date = new Date(2017, 11, 25);
 
+    // Setting the selected date
+    selectedDate: Date = new Date(2017, 10, 20);
+
+
     constructor() { }
 
     ngOnInit() {
